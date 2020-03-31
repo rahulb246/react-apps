02e5aee2674e7f9f6b7b1a9527f57e58c0b6d483
Rahul Bojanapally <rahul.b246@gmail.com>
2020-03-31 19:16:05 +0530

    removed client ID

--- 4-pics/src/api/unsplash.js
+++ 4-pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID Eyq4X2Z2GAM_dy7zaiIgcMS4zFayIl3ocCsUYKPANos'
+        Authorization: 'Client-ID XXXXXXXcreate your own ID and put it here XXXXXXXXX'
     }
 });
