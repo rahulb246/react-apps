8e39d467c9842bb31f49b279ac87868ce1557def
Rahul Bojanapally <rahul.b246@gmail.com>
2020-04-01 23:25:35 +0530

    Update youtube.js

--- 5-videos/src/apis/youtube.js
+++ 5-videos/src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAFzMreoda2bZ9ZLS-Uui3uhLquKnONK5M';
+const KEY = 'xxxxxxxxxxxxxx';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
